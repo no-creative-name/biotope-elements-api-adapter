83a09d499406a6e562d44893ad0ee1b643ed61c1
Benjamin Konopka <benjamin.konopka@virtual-identity.com>
2019-06-12 10:54:06 +0200

    add adapter for wordpress

--- src/resources/ts/apiAdapter/WPGraphQL/getDataByContentId.ts
+++ src/resources/ts/apiAdapter/WPGraphQL/getDataByContentId.ts
@@ -2,11 +2,34 @@ const getDataByContentId = async (
   contentId: number,
   nestedLevel: number = 0
 ) => {
+  const query = `
+    {
+      pageBy(id: "cGFnZToy") {
+        slug
+      }
+    }
+  `
+  console.log("TCL: query", query)
+
+
+  fetch(CMSAPI, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({query})
+  })
+  .then(response => response.json())
+  .then((responseJson) => {
+    console.log(responseJson)
+  });
+
   return {
-    title: "",
-    breadcrumb: "",
-    children: []
-  };
+    title: '',
+    breadcrumb: '',
+    children:[]
+  }
 };
 
 export default getDataByContentId;
