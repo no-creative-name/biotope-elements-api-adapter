1232de726fd0a18fd9c74f081994c6f9fa36963f
Kai Wißler <kai.wissler@schwarzfisch.virtual-identity.net>
2019-06-13 10:50:03 +0200

    feat: Remove leftover mappers

--- src/resources/ts/dataMapper/performDataMapping.ts
+++ src/resources/ts/dataMapper/performDataMapping.ts
@@ -1,26 +1,7 @@
 import { Content } from "../interfaces/Content";
-import { accordionMapper } from "./accordion/mapper";
-import { accordionItemMapper } from "./accordionItem/mapper";
-import { downloadListMapper } from "./downloadList/mapper";
-import { textMediaInlineMapper } from "./textMediaInline/mapper";
-import { articleTileMapper } from "./articleTile/mapper";
-import { articleTileItemMapper } from "./articleTileItem/mapper";
 import { primaryCtaMapper } from "./primaryCTA/mapper";
-import { secondaryCtaMapper } from "./secondaryCTA/mapper";
 import { leadTextMapper } from "./leadText/mapper";
 import { imageTextMapper } from "./imageText/mapper";
-import { fileMapper } from "./file/mapper";
-import { genericListMapper } from "./genericList/mapper";
-import { genericListItemMapper } from "./genericListItem/mapper";
-import { sliderMapper } from "./slider/mapper";
-import { sliderItemMapper } from "./sliderItem/mapper";
-import { stageMapper } from "./stage/mapper";
-import { tabContainerMapper } from "./tabContainer/mapper";
-import { tabItemMapper } from "./tabItem/mapper";
-import { routingTeaserMapper } from "./routingTeaser/mapper";
-import { routingTeaserItemMapper } from "./routingTeaserItem/mapper";
-import { teaserRowMapper } from "./teaserRow/mapper";
-import { teaserRowItemMapper } from "./teaserRowItem/mapper";
 
 const contentTypeComponentMap = {
   leadText: leadTextMapper,
